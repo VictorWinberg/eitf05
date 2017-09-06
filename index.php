d572a5e6ac0d2ef121be782960496ce98a5e0566
VictorWinberg <dat14vwi@student.lu.se>
2017-09-06 15:28:42 +0200

    DATABASE_URL

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 include 'env.php';
 require 'utility.php';
 
-$mysqli = new mysqli(
-  getenv("HOST"),
-  getenv("USER"),
-  getenv("PASSWORD"),
-  getenv("DB")
-);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+debug([$server, $username, $password, $db]);
+
+$conn = new mysqli($server, $username, $password, $db);
 ?>
 
 <html>
@@ -17,17 +21,17 @@ $mysqli = new mysqli(
   <body>
     <?php
     echo '<h1>Web Shop Under Attack</h1>';
+    echo '<h3>Host: ' . $server . '</h3>';
+    echo '<h2>Tables</h2>';
 
-    echo '<h3>Host: ' . getenv("HOST") . '</h3>';
-
-    $tables = $mysqli->query("SHOW TABLES");
+    $tables = $conn->query("SHOW TABLES");
     debug($tables);
     while($tableName = mysqli_fetch_row($tables)) {
       $table = $tableName[0];
       debug($table);
       echo '<h3>' . $table . '</h3>';
 
-      $columns = $mysqli->query("SHOW COLUMNS from " . $table);
+      $columns = $conn->query("SHOW COLUMNS from " . $table);
       debug($columns);
 
       if(mysqli_num_rows($columns)) {
