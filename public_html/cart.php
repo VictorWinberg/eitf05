e47f308441a7af77a7612cb088e572a257f0a25e
Daniel Tovesson <daniel.tovesson@me.com>
2017-09-12 16:39:02 +0200

    Adding table with cart items on cart.php (dummy data)

--- public_html/cart.php
+++ public_html/cart.php
@@ -1,5 +1,21 @@
 <link rel="stylesheet" href="css/index.css" />
 
+<?php
+
+// TODO Dummy data
+$cart = array(
+	array("id" => 1, "name" => "Fidget spinner", "price" => 0.90, "quantity" => 1),
+	array("id" => 2, "name" => "Fidget spinner edition", "price" => 1999.9, "quantity" => 2),
+	array("id" => 3, "name" => "Mjölk", "price" => 10.79, "quantity" => 4)
+);
+
+$total = 0;
+foreach($cart as $item) {
+	$total += ($item["price"] * $item["quantity"]);
+}
+
+?>
+
 <html>
 	<?php require_once('header.php'); ?>
 	<body>
@@ -7,6 +23,30 @@
 		<?php require_once('navigationBar.php'); ?>
 
 		<h1>Kundvagn</h1>
+
+		<form>
+			<table>
+				<tr>
+					<th>Namn</th>
+					<th>Á-pris</th>
+					<th>Antal</th>
+					<th>Totalt</th>
+				</tr>
+				<?php foreach($cart as $item) { ?>
+					<tr>
+						<td><?php echo $item["name"]; ?></td>
+						<td><?php echo $item["price"]; ?></td>
+						<td><input type="text" value="<?php echo $item["quantity"]; ?>"></td>
+						<td><?php echo ($item["price"] * $item["quantity"]); ?></td>
+						<td><input type="submit" value="Ta bort"></td>
+					</tr>
+				<?php } ?>
+			</table>
+			<p>
+				<b>Summa:</b> <?php echo $total; ?>
+			</p>
+			<input type="submit" value="Betala">
+		</form>
 
 	</body>
 </html>
