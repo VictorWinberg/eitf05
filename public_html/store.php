592a7d7f976c96d39930bafd15284c6071bbe30f
Daniel Tovesson <daniel.tovesson@me.com>
2017-09-12 16:29:29 +0200

    Adding table with products to store.php (dummy data)

--- public_html/store.php
+++ public_html/store.php
@@ -1,5 +1,16 @@
 <link rel="stylesheet" href="css/index.css" />
 
+<?php
+
+// TODO Dummy data
+$products = array(
+	array("id" => 1, "name" => "Fidget spinner", "price" => 0.90),
+	array("id" => 2, "name" => "Fidget spinner edition", "price" => 1999.9),
+	array("id" => 3, "name" => "Mjölk", "price" => 10.79)
+);
+
+?>
+
 <html>
 	<?php require_once('header.php'); ?>
 	<body>
@@ -7,6 +18,24 @@
 		<?php require_once('navigationBar.php'); ?>
 
 		<h1>Produkter</h1>
+
+		<form>
+			<table>
+				<tr>
+					<th>Namn</th>
+					<th>Pris</th>
+				</tr>
+				<?php foreach($products as $product) { ?>
+					<tr>
+						<td><?php echo $product["name"]; ?></td>
+						<td><?php echo $product["price"]; ?></td>
+						<td><input type="checkbox" value="<?php echo $product["id"]; ?>"></td>
+					</tr>
+				<?php } ?>
+			</table>
+			<br/>
+			<input type="submit" value="Lägg till i varukorgen">
+		</form>
 
 	</body>
 </html>
