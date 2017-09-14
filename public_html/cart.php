9d6c5fed09c5bc896cd50a5de72065e7ed3a5037
Daniel Tovesson <daniel.tovesson@me.com>
2017-09-14 21:20:10 +0200

    Adding redirect to login.php on cart.php and profile.php if user is logged out

--- public_html/cart.php
+++ public_html/cart.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php
+session_start();
+if (!isset($_SESSION['logged_in'])) {
+	header("location: login.php");
+}
+?>
 
 <?php require 'connect.php' ?>
 
--- public_html/profile.php
+++ public_html/profile.php
@@ -1,4 +1,10 @@
-<?php session_start(); ?>
+<?php
+session_start();
+if (!isset($_SESSION['logged_in'])) {
+	header("location: login.php");
+}
+?>
+
 <?php require 'connect.php' ?>
 
 <!-- Get orders from database -->
