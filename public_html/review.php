e57d8d5be04dd0d0d701445c946947bb333cecfa
VictorWinberg <dat14vwi@student.lu.se>
2017-10-11 13:19:08 +0200

    Formatting and deleted unused sites

--- public_html/review.php
+++ public_html/review.php
@@ -4,6 +4,7 @@ if (!isset($_SESSION['logged_in'])) {
 	header("location: index.php");
 }
 ?>
+
 <?php
 require 'connect.php';
 require 'utility.php';
@@ -13,35 +14,45 @@ isset($_POST['csrf_token']) && $_SESSION['csrf_token'] == $_POST['csrf_token']) 
 	if (!isset($_SESSION['logged_in'])) {
 		exit();
   }
+
   $subject = htmlspecialchars($_POST['subject'], ENT_QUOTES);
   $review = htmlspecialchars($_POST['review'], ENT_QUOTES);
   $username = $_SESSION['login_user']['name'];
+
   $sql = "INSERT INTO Reviews (name, subject, review)
   VALUES (?,?,?)";
+
   if($statement=$conn->prepare($sql)) {
     $statement->bind_param("sss", $username, $subject, $review);
-    if($statement->execute()) {
-    } else {
-      $error="unable to execute mysqli query";
-    }
+
+		if(!$statement->execute())
+      $error = "Unable to submit the review, please try again.";
+
   } else {
-    $error="unable to execute mysqli query";
+    $error = "Unable to submit the review, please try again.";
   }
 }
-$reviews = getReviews($conn);
 ?>
+
+<!-- Get reviews from database -->
+<?php $reviews = getReviews($conn); ?>
+
 <html>
   <body>
     <?php require('header.php'); ?>
+
     <form action="" method="POST">
       <?php require('navigationBar.php'); ?>
-      <h3>Leave your review of Spin and Milk here:</h3>
+
+      <h3>Leave your review of Fidget Express here:</h3>
+
       <input type="text" name="subject" placeholder="Subject" maxlength="40"/>
       <br/><br />
       <textarea name='review' rows="5" cols="80" id='comment'></textarea><br />
       <br/><br />
       <button class="btn" style="width: 250" type="submit">Submit</button>
       <br/><br />
+
       <?php if (count($reviews)) { ?>
         <?php foreach($reviews as $item) {
           $date=explode(" ", $item['ts']);
@@ -53,6 +64,7 @@ $reviews = getReviews($conn);
           </div>
         <?php } ?>
       <?php } ?>
+
       <p style="font-size: 0.8em; color: DarkSlateGray"></p>
       <p class="small" style="font-size: 0.8em; color:red">
         <?php if(isset($error)) echo $error; ?>
